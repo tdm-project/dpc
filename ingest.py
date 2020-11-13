6fdcf4ce211b36b80df5e28835f1e9b50e4d6a75
Mauro Del Rio <mauro@crs4.it>
2020-11-13 17:00:28 +0100

    add python files

---
+++ ingest.py
@@ -0,0 +1,99 @@
+import json
+import logging
+import os
+from datetime import datetime, timedelta
+
+import dateparser
+import numpy as np
+from clize import parameters, run
+from tdmq.client import Client
+
+from dpc import fetch_dpc_data
+
+logger = None
+
+
+def setup_logging(level_name):
+    global logger
+    level = getattr(logging, level_name)
+
+    logging.basicConfig(level=level)
+    logger = logging.getLogger('setup_source')
+    logger.info('Logging is active (level %s).', level_name)
+
+
+def load_desc(opt):
+    source_dir = 'sources'
+    if opt == 'radar':
+        fname = os.path.join(source_dir, 'dpc-meteoradar.json')
+    elif opt == 'temperature':
+        fname = os.path.join(source_dir, 'dpc-temperature.json')
+    else:
+        assert False
+    full_path = os.path.join(os.path.dirname(__file__), fname)
+    logging.debug('loading source description from %s.', full_path)
+    with open(full_path) as f:
+        return json.load(f)
+
+
+def create_timebase(when, dt_back):
+    # We align on the hourly edge
+    return (datetime(when.year, when.month, when.day, when.hour) - dt_back)
+
+
+def main(source: parameters.one_of('radar', 'temperature'),
+         *,
+         tdmq_url: ('u'),
+         start='6 days ago',
+         end='now',
+         log_level: ('v',
+                     parameters.one_of("DEBUG", "INFO", "WARN", "ERROR",
+                                       "CRITICAL")) = 'INFO'):
+    setup_logging(log_level)
+
+    desc = load_desc(source)
+
+    dt = timedelta(seconds=desc['description']['acquisition_period'])
+
+    c = Client(tdmq_url)
+    srcs = c.find_sources({'id': desc['id']})
+    if len(srcs) > 0:
+        assert len(srcs) == 1
+        s = srcs[0]
+        logger.info(f"Using source {s.tdmq_id} for {s.id}.")
+    else:
+        s = c.register_source(desc)
+        logger.info(f"Created source {s.tdmq_id} for {s.id}.")
+
+    start = dateparser.parse(start)
+    end = dateparser.parse(end)
+    try:
+        ts = s.timeseries(after=start, before=end)
+    except Exception as ex:  # FIXME too general
+        ts = []
+    # The DPC source keeps data available for only one week
+    time_base = ts.time[0] if len(ts) > 0 else create_timebase(
+        end, end - start)
+    times = np.arange(time_base, end, dt)
+    filled = times[np.searchsorted(times, ts.time)]\
+        if len(ts) > 0 else []
+    to_be_filled = set(times) - set(filled)
+
+    for t in to_be_filled:
+        t = t if isinstance(t, datetime) else t.tolist()
+        data = {}
+        for f in s.controlled_properties:
+            data[f] = fetch_dpc_data(s, t, f)
+        slot = int((t - time_base).total_seconds() // dt.total_seconds())
+        logger.info(f"Ingesting data at time {t}, slot {slot}.")
+        try:
+            s.ingest(t, data, slot)
+        except Exception as e:
+            logger.error(
+                'an error occurred when ingesting time %s at slot %s.'
+                'Exception: %s', t, slot, e)
+    logger.info(f"Done ingesting.")
+
+
+if __name__ == "__main__":
+    run(main)
