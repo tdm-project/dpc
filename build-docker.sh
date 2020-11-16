5cb70d1899dedf477f8eaff91d7ef92dac0bfddb
Mauro Del Rio <mauro@crs4.it>
2020-11-16 16:18:31 +0100

    version working with s3

--- build-docker.sh
+++ build-docker.sh
@@ -2,7 +2,6 @@
 
 mkdir -p docker/dpc
 cp ingest.py docker/dpc
-cp dpc.py docker/dpc
 cp -r sources docker/dpc
 cp requirements.txt docker/dpc
 docker build -t tdmproject/dpc_ingestor -f docker/Dockerfile docker
